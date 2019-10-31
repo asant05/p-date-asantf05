9f7ddece85f22e6bd3209fff62b25221703e61ad
Ainoa <asantf05@estudiantes.unileon.es>
2019-10-31 14:05:25 +0100

    Primera

---
+++ src/es/unileon/prg1/pdate/PDatePrincipal.java
@@ -0,0 +1,15 @@
+package es.unileon.prg1.pdate;
+
+public class PDatePrincipal{
+	public static void main(String[]args ) {
+		Date myFecha= new Date();
+		Date myFecha2= new Date(30,10,2019);
+		if(myFecha.isSameYear(myFecha2)) {
+			System.out.println("Es la misma fecha");
+		}
+		else {
+			System.out.println("No es la misma fecha");
+		}
+		System.out.println(myFecha);
+	}
+}
